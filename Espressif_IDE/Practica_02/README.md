820df912444810c192626e9bd089e78e4b65f1d2
CarlosSHT_HomeW10 <carlos.herrera.trujillo@gmail.com>
2024-05-29 01:57:50 -0500

    Practica 02 IDE. Creacion de proyecto & Actividad 1.1

---
+++ Espressif_IDE/Practica_02/README.md
@@ -0,0 +1,15 @@
+Práctica 02 - MSE Curso: Sistemas Embebidos Distribuidos
+====================
+
+Alumno: Carlos Herrera Trujillo
+
+
+Actividad 1: Lectura del sensor
+-------------------------------
+
+Actividad de montaje de LDR y verificación de lecturas.
+
+	1. Tarjeta de conexión con GPIO34 para la lectura del LDR (en serie con resistencia de 10K) y 
+	GPIO25 para la emisión PWM en Led Rojo.
+ 
+<img src="./imgs/fig1.png" alt="Texto alternativo" width="300">
---
+++ Espressif_IDE/Practica_02/main/config.h
@@ -0,0 +1,27 @@
+/** \file config.h
+ *  May 2024
+ *  Maestría en SIstemas Embebidos - Sistemas embebidos distribuidos
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+/* versión ESP-IDF */
+//#define ESPIDF_v4 // comentar esta línea para la versión 5
+
+/* configuración WIFI */
+#define ESP_WIFI_SSID "ssid"     // CONFIG_ESP_WIFI_SSID  // "SSID wifi"
+#define ESP_WIFI_PASS "pass"     // CONFIG_ESP_WIFI_PASS  // "pass wifi"
+
+/*  Configuracion MQTT  */
+#define PORT_MQTT 1883            //CONFIG_PORT_MQTT      // default
+#define IP_BROKER_MQTT "broker"    //CONFIG_IP_BROKER_MQTT // Broker MQTT
+#define USER_MQTT "usuario"                   // Usuario de MQTT
+#define PASSWD_MQTT "usuariopassword"         // Contraseña de MQTT
+
+/*  configuración IO */
+#define BLINK_GPIO CONFIG_BLINK_GPIO  // port 2 para NodeMcu-23S
+#define PWM_PORT 25          // puerto de salida de pwm
+
+
+#endif
